72d658f98dbd7176290204e44d6f6cf4e05833b8
Jonathan Vale <creativeweb.de@gmail.com>
2023-07-25 20:57:02 -0300

    test flask return

---
+++ api/entities.py
@@ -0,0 +1,5 @@
+class Locations(object):
+    def __init__(self, name, address, image):
+        self.name = name
+        self.address = address
+        self.image = image
--- api/index.py
+++ api/index.py
@@ -1,3 +1,4 @@
+from api.entities import Locations
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,3 +10,11 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route("/locations")
+def summary():
+    d = Locations()
+    d.name = 'ABC',
+    d.address = 'avc'
+    d.image = 'vvv'
+    return d
