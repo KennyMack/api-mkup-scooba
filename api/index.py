799d9f0b538e3f5344d889fcd33e3e1b2a1646e8
Jonathan Vale <creativeweb.de@gmail.com>
2023-07-25 22:32:24 -0300

    all json created

--- api/index.py
+++ api/index.py
@@ -6,11 +6,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+def getJsonData(file):
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "static/data", file)
+    data = json.load(open(json_url))
+    return data
+
+@app.route("/foods")
+def foods():
+    return jsonify(getJsonData("foods.json")), 200
+
+@app.route("/musics")
+def musics():
+    return jsonify(getJsonData("musics.json")), 200
+
+@app.route("/products")
+def products():
+    return jsonify(getJsonData("products.json")), 200
+
+@app.route("/souvenirs")
+def souvenirs():
+    return jsonify(getJsonData("souvenirs.json")), 200
 
 @app.route("/locations")
 def locations():
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, "static/data", "locations.json")
-    data = json.load(open(json_url))
-    return jsonify(data), 200
+    return jsonify(getJsonData("locations.json")), 200
